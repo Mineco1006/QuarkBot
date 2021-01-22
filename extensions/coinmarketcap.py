e26b880f9fd4012d489278635234aa1351771614
Mineco1006 <57864185+Mineco1006@users.noreply.github.com>
2021-01-22 02:28:32 +0100

    Add files via upload

--- extensions/coinmarketcap.py
+++ extensions/coinmarketcap.py
@@ -3,10 +3,15 @@ from discord.ext import commands
 import configparser
 from coinmarketcapapi import CoinMarketCapAPI
 import locale
+from datetime import datetime
+import logging
+
+logging.basicConfig(filename=".bot.log", level=logging.INFO)
 
 config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 config.read('./config.ini')
 cmc = CoinMarketCapAPI(config.get('CMC', 'apikey'))
+locale.setlocale(locale.LC_ALL, '')
 
 class coinmarketcap(commands.Cog):
 
@@ -18,7 +23,9 @@ class coinmarketcap(commands.Cog):
         r = cmc.cryptocurrency_quotes_latest(symbol=symbol)
         d = r.data.get(symbol.upper())
         u = d.get("quote").get("USD")
-        print(d.get('name'))
+        name = d.get('name')
+        print(f'[{datetime.now()}]Cryptocurrency quote {name} requested by {ctx.message.author}')
+        logging.info(f'[{datetime.now()}]Cryptocurrency quote {name} requested by {ctx.message.author}')
 
         embed= discord.Embed(
             title=d.get('name').upper(),
@@ -26,15 +33,15 @@ class coinmarketcap(commands.Cog):
                         'Volume 24h: ${}\n'
                         'Change 24h: {}%\n'
                         'Supply: {} {}\n'
-                        'Market capitalization: ${}\n'.format(locale.format_string('%.4f', float(u.get("price")), grouping=True, monetary=True),
-                                                            locale.format_string('%.2f', float(u.get("volume_24h")), grouping=True, monetary=True),
+                        'Market capitalization: ${}\n'.format(locale.format_string('%.4f', float(u.get("price")), grouping=True),
+                                                            locale.format_string('%.2f', float(u.get("volume_24h")), grouping=True),
                                                             locale.format_string('%.2f', float(u.get("percent_change_24h"))),
                                                             locale.format_string('%.0f', float(d.get("circulating_supply")), grouping=True),
                                                             d.get("symbol"),
-                                                            locale.format_string('%.0f', float(u.get("market_cap")), grouping=True, monetary=True)),
-            colour= discord.Colour.blue()
+                                                            locale.format_string('%.0f', float(u.get("market_cap")), grouping=True)),
+            colour=discord.Colour.blue()
         )
-        await ctx.send(embed= embed)
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(coinmarketcap(client))
--- extensions/reactionroles.py
+++ extensions/reactionroles.py
@@ -1,6 +1,10 @@
 import discord
 import configparser
 from discord.ext.commands import Cog
+from datetime import datetime
+import logging
+
+logging.basicConfig(filename=".bot.log", level=logging.INFO)
 
 config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 
@@ -27,12 +31,15 @@ class reactionroles(Cog):
             verifemb = config['VERIFIEDEMBED']
             embed = discord.Embed(
                 title=verifemb['title'],
+                description=verifemb['description'],
                 colour=discord.Colour.blue()
             )
 
             await member.send(embed=embed)
 
-            print('User {} is now verified'.format(member))
+            print(f'[{datetime.now()}]User {member} is now verified')
+            logging.info(f'[{datetime.now()}]User {member} is now verified')
+
 
         if message_id == lang_id:
             if payload.emoji.name == "🇩🇪":  # german role
@@ -40,49 +47,57 @@ class reactionroles(Cog):
 
                 await member.add_roles(role)
 
-                print('{} now has the role {}'.format(member,role))
+                print(f'[{datetime.now()}]{member} now has the role {role}')
+                logging.info(f'[{datetime.now()}]{member} now has the role {role}')
             if payload.emoji.name == "🇷🇺":  # russian role
                 role = discord.utils.get(guild.roles, id=int(role_id['rus']))
 
                 await member.add_roles(role)
 
-                print('{} now has the role {}'.format(member,role))
+                print(f'[{datetime.now()}]{member} now has the role {role}')
+                logging.info(f'[{datetime.now()}]{member} now has the role {role}')
             if payload.emoji.name == "🇫🇷":  # french role
                 role = discord.utils.get(guild.roles, id=int(role_id['fra']))
 
                 await member.add_roles(role)
 
-                print('{} now has the role {}'.format(member,role))
+                print(f'[{datetime.now()}]{member} now has the role {role}')
+                logging.info(f'[{datetime.now()}]{member} now has the role {role}')
             if payload.emoji.name == "🇪🇸":  # spanish role
                 role = discord.utils.get(guild.roles, id=int(role_id['esp']))
 
                 await member.add_roles(role)
 
-                print('{} now has the role {}'.format(member,role))
+                print(f'[{datetime.now()}]{member} now has the role {role}')
+                logging.info(f'[{datetime.now()}]{member} now has the role {role}')
             if payload.emoji.name == "🇨🇳":  # chinese role
                 role = discord.utils.get(guild.roles, id=int(role_id['cn']))
 
                 await member.add_roles(role)
 
-                print('{} now has the role {}'.format(member,role))
+                print(f'[{datetime.now()}]{member} now has the role {role}')
+                logging.info(f'[{datetime.now()}]{member} now has the role {role}')
             if payload.emoji.name == "🇮🇳":  # hindi role
                 role = discord.utils.get(guild.roles, id=int(role_id['hin']))
 
                 await member.add_roles(role)
 
-                print('{} now has the role {}'.format(member,role))
+                print(f'[{datetime.now()}]{member} now has the role {role}')
+                logging.info(f'[{datetime.now()}]{member} now has the role {role}')
             if payload.emoji.name == "🇰🇷":  # korean role
                 role = discord.utils.get(guild.roles, id=int(role_id['kor']))
 
                 await member.add_roles(role)
 
-                print('{} now has the role {}'.format(member,role))
+                print(f'[{datetime.now()}]{member} now has the role {role}')
+                logging.info(f'[{datetime.now()}]{member} now has the role {role}')
             if payload.emoji.name == "🇯🇵":  # japanese role
                 role = discord.utils.get(guild.roles, id=int(role_id['jap']))
 
                 await member.add_roles(role)
 
-                print('{} now has the role {}'.format(member,role))
+                print(f'[{datetime.now()}]{member} now has the role {role}')
+                logging.info(f'[{datetime.now()}]{member} now has the role {role}')
 
     @Cog.listener()
     async def on_raw_reaction_remove(self, payload):
@@ -90,15 +105,18 @@ class reactionroles(Cog):
         guild = await self.client.fetch_guild(payload.guild_id)
         member = await guild.fetch_member(payload.user_id)
         config.read('./config.ini')
-        verify_id = int(config.get('MESSAGEID','verify'))
+        verify_id = int(config.get('MESSAGEID', 'verify'))
         lang_id = int(config.get('MESSAGEID', 'lang'))
         role_id = config['ROLEID']
 
 
-        if message_id == verify_id:  # verification
-            role = discord.utils.get(guild.roles, id=int(role_id['verified']))
+        if message_id == verify_id:  # (un)verification
 
-            await member.remove_roles(role)
+            everyone = guild.default_role
+            roles = member.roles
+            roles.remove(everyone)
+
+            await member.remove_roles(*roles)
 
             embed = discord.Embed(
                 title=config.get('UNVERIFIEDEMBED', 'title'),
@@ -107,7 +125,8 @@ class reactionroles(Cog):
             )
             await member.send(embed=embed)
 
-            print('Role {} was removed from {}'.format(role, member))
+            print(f'[{datetime.now()}]Role verified and others were removed from {member}')
+            logging.info(f'[{datetime.now()}]Role verified and others were removed from {member}')
 
         if message_id == int(lang_id):
             if payload.emoji.name == "🇩🇪":  # german role
@@ -115,50 +134,58 @@ class reactionroles(Cog):
 
                 await member.remove_roles(role)
 
-                print('Role {} was removed from {}'.format(role, member))
+                print(f'[{datetime.now()}]Role {role} was removed from {member}')
+                logging.info(f'[{datetime.now()}]Role {role} was removed from {member}')
             if payload.emoji.name == "🇷🇺":  # russian role
                 role = discord.utils.get(guild.roles, id=int(role_id['rus']))
 
                 await member.remove_roles(role)
 
-                print('Role {} was removed from {}'.format(role, member))
+                print(f'[{datetime.now()}]Role {role} was removed from {member}')
+                logging.info(f'[{datetime.now()}]Role {role} was removed from {member}')
             if payload.emoji.name == "🇫🇷":  # french role
                 role = discord.utils.get(guild.roles, id=int(role_id['fra']))
 
                 await member.remove_roles(role)
 
-                print('Role {} was removed from {}'.format(role, member))
+                print(f'[{datetime.now()}]Role {role} was removed from {member}')
+                logging.info(f'[{datetime.now()}]Role {role} was removed from {member}')
             if payload.emoji.name == "🇪🇸":  # spanish role
                 role = discord.utils.get(guild.roles, id=int(role_id['esp']))
 
                 await member.remove_roles(role)
 
-                print('Role {} was removed from {}'.format(role, member))
+                print(f'[{datetime.now()}]Role {role} was removed from {member}')
+                logging.info(f'[{datetime.now()}]Role {role} was removed from {member}')
             if payload.emoji.name == "🇨🇳":  # chinese role
                 role = discord.utils.get(guild.roles, id=int(role_id['cn']))
 
                 await member.remove_roles(role)
 
-                print('Role {} was removed from {}'.format(role, member))
+                print(f'[{datetime.now()}]Role {role} was removed from {member}')
+                logging.info(f'[{datetime.now()}]Role {role} was removed from {member}')
             if payload.emoji.name == "🇮🇳":  # hindi role
                 role = discord.utils.get(guild.roles, id=int(role_id['hin']))
 
                 await member.remove_roles(role)
 
-                print('Role {} was removed from {}'.format(role, member))
+                print(f'[{datetime.now()}]Role {role} was removed from {member}')
+                logging.info(f'[{datetime.now()}]Role {role} was removed from {member}')
             if payload.emoji.name == "🇰🇷":  # korean role
                 role = discord.utils.get(guild.roles, id=int(role_id['kor']))
 
                 await member.remove_roles(role)
 
-                print('Role {} was removed from {}'.format(role, member))
+                print(f'[{datetime.now()}]Role {role} was removed from {member}')
+                logging.info(f'[{datetime.now()}]Role {role} was removed from {member}')
             if payload.emoji.name == "🇯🇵":  # japanese role
                 role = discord.utils.get(guild.roles, id=int(role_id['jap']))
 
                 await member.remove_roles(role)
 
 
-                print('Role {} was removed from {}'.format(role, member))
+                print(f'[{datetime.now()}]Role {role} was removed from {member}')
+                logging.info(f'[{datetime.now()}]Role {role} was removed from {member}')
 
 def setup(client):
     client.add_cog(reactionroles(client))
